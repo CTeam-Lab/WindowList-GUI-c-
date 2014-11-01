095de20ceca5442c7c2f43bb055c89cf231e2a04
Jorge Meireles <jmeireles@rocket-internet.pt>
2014-11-01 19:46:26 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include "WindowList.h"
 #include <gtk/gtk.h>
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
 	WindowList gui;
 	gtk_init (&argc, &argv);
-
 	gui.create();
-
 	gtk_main();
-
 	return 0;
 }
